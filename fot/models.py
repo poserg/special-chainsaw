ea8b495fd94e6c5023d5dbed0f1cb207fefca0b9
Sergei Popov <poserg@gmail.com>
2023-03-01 06:17:25 +0300

    Increase length of forecast name

--- fot/models.py
+++ fot/models.py
@@ -97,7 +97,7 @@ class Wish(Timestamped):
 
 
 class Forecast(Timestamped):
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.name}"
